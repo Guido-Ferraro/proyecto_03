209cebfebd91892bc6aabc914ddd1a7dc9d6de5b
Guido-Ferraro <gdoferraro@gmail.com>
2021-08-20 09:47:02 -0300

    Added nav sections intro header style and installed fade in component package. Added fade in component to clients component

--- components/Companies.tsx
+++ components/Companies.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "../styles/Companies.module.css";
-
+import FadeIn from "react-fade-in/lib/FadeIn";
 const Companies = (): JSX.Element => {
   function importAll(r: any) {
     return r.keys().map(r);
@@ -11,21 +11,23 @@ const Companies = (): JSX.Element => {
   );
 
   const imageList = images.map(
-    (element: {
-      default: { src: string; width: number; height: number };
-    }): any => {
+    (
+      element: { default: { src: string; width: number; height: number } },
+      index: number
+    ): any => {
       console.log(element);
       const { src, width, height } = element.default;
       const ratio: number = width / height;
       return (
-        <Image
-          src={src}
-          alt=""
-          key={src}
-          width={ratio * 75}
-          height={75}
-          className={styles.logo}
-        />
+        <FadeIn delay={600 + 200 * index} transitionDuration={1000} key={src}>
+          <Image
+            src={src}
+            alt=""
+            width={ratio * 75}
+            height={75}
+            className={styles.logo}
+          />
+        </FadeIn>
       );
     }
   );
@@ -34,8 +36,12 @@ const Companies = (): JSX.Element => {
     <section className={styles.section}>
       <div className={styles.container}>
         <div className={styles.text}>
-          <div className={styles.label}>Acerca de nostros</div>
-          <h2 className={styles.title}>Compañías con las que trabajamos</h2>
+          <FadeIn delay={200} transitionDuration={1000}>
+            <div className={styles.label}>Acerca de nostros</div>
+          </FadeIn>
+          <FadeIn delay={400} transitionDuration={1000}>
+            <h2 className={styles.title}>Compañías con las que trabajamos</h2>
+          </FadeIn>
         </div>
         <div className={styles.slider}>{imageList}</div>
       </div>
--- components/NavSectionIntro.tsx
+++ components/NavSectionIntro.tsx
@@ -11,6 +11,9 @@ const NavSectionIntro = (props: NavSectionIntroProps): JSX.Element => {
     if (box?.style.height != null) {
       box.style.transition = "1s";
       box.style.height = "100px";
+      box.style.background = "black";
+      box.style.background =
+        "linear-gradient(45deg, transparent 10%, black, transparent 75%)";
     }
   }, []);
 
